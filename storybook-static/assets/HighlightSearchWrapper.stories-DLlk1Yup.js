3dc5d0512790fe9895b411e5c4a1d50f8ea9a360
Vladislav Dotsenko <speedway.artist@gmail.com>
2024-12-29 20:37:43 +0100

    Update storybook pages

---
+++ storybook-static/assets/HighlightSearchWrapper.stories-DLlk1Yup.js
@@ -0,0 +1,8 @@
+import{R as o}from"./index-Cp6HKLZC.js";import{H as n}from"./HighlightSearchWrapper-Cr0wFqbd.js";const l={title:"Example/HighlightSearchWrapper",component:n,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{}},e={args:{index:0,searchString:"Hello",children:o.createElement("div",null,"Hello World!")}};var r,a,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    index: 0,
+    searchString: "Hello",
+    // onMatchData: ({wrapperIndex, matchesFound, matchParentElement}) => {},
+    children: <div>Hello World!</div>
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const i=["Primary"];export{e as Primary,i as __namedExportsOrder,l as default};
